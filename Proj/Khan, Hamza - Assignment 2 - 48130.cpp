689d40f73312f202b2a02d1dc4a43c94559b3dae
trl10254 <hamzajkhan@gmail.com>
2014-09-17 19:18:24 -0700

    homework

---
+++ Proj/Khan, Hamza - Assignment 2 - 48130.cpp
@@ -0,0 +1,36 @@
+//Khan, Hamza - Assignment 2 - 48130
+//Created on September 17, 2014
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	float mean(int[integer],int),
+	      median(int[integer],int)
+	      firstnumber;
+	int   mode(int[integer],int);
+	int integer;
+    
+    //State the purpose of the program
+    cout << "The purpose of this program is to calculate the mean,
+             median, and mode of a given set of numbers" << endl;
+
+    //Asks the user how many numbers does the array have to go
+    cout << "What is the limit you want to set for your calcuations: ";
+    cin >> integer;
+
+    //Ask what is the first number in the array
+    cout << "What is the first number in your limit: ";
+    cin >> firstnumber;
+
+    //Calculation to find the mean
+    for (int count = 0; count < integer; count++)
+    {
+     mean = firstnumber 
+    }
+
+
+    return 0;
+}
