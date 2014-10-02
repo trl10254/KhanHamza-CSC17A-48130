de8eea16fa8a7bd8e62bf4c6519e5578e17b2c89
trl10254 <hamzajkhan@gmail.com>
2014-10-02 14:15:27 -0700

    Signed-off-by: trl10254 <hamzajkhan@gmail.com>

--- Hmwk/Chapter 10 Homework/main.cpp
+++ Hmwk/Chapter 10 Homework/main.cpp
@@ -13,11 +13,16 @@ using namespace std;
 
 //Declare function prototypes
 int length(char *chararray);
+void backward(char *chararray);
+
 void Chapter10Problem1();
+void Chapter10Problem2();
+void Chapter10Problem3();
+void Chapter10Problem4();
+void Chapter10Problem5();
 
 int main()
 {
-   
     
     return 0;
 }
@@ -32,7 +37,7 @@ int main()
 void Chapter10Problem1()
 {
     
-const int SIZE = 50;
+    int SIZE = 50;
     char sentence[SIZE];
     int count;
     
@@ -42,13 +47,12 @@ const int SIZE = 50;
     cin.getline(sentence, SIZE);
     
     //Get the length of the sentence
-    count= length(sentence);
+    count = length(sentence);
     
     //Display the total number of characters
     cout << "In your sentence you have a total of " << count; 
     cout << " characters in it." << endl;
-    
-    return Chapter10Problem1;
+   
 }
     
 int length(char *chararray)
@@ -60,7 +64,7 @@ int length(char *chararray)
     //Do a for loop to get the total length
     for (int i = 0; i <= length; i++)
     {
-        if (chararray[i] == ' ')
+        if (chararray[i] != ' ')
         {
             spacecount++;
         }
@@ -68,3 +72,41 @@ int length(char *chararray)
     
 }
 
+//Chapter 10 Problem 2: Write a function that accepts a pointer to a C-string as  
+//an argument and displays its content backward. For instance, if the string 
+//argument is "Gravity" the function should display "ytivarG". Demonstrate the 
+//function in a program that asks the user to input a string and then passes it  
+//to the function.
+
+void Chapter10Problem2()
+{
+    //Declare local variables
+    int SIZE = 100;
+    char sentence[SIZE];
+    char reflection;
+    
+    cin.ignore();
+    //Get the user to input a line or a word
+    cout << "Please enter a phrase or word you want to be read backwards" << endl;
+    cin.getline(sentence, SIZE);
+    
+    //Get the reflection
+    backward(sentence);
+    
+}
+
+void backward(char *chararray)
+{
+    //Declare local variables
+    int stringlength = length(chararray);
+    
+    cout << "The phrase read backwards is: ";
+    
+    //Display the backward phase
+    for(int i = (stringlength + 1); i >= 0; i--)
+    {
+        cout << chararray[i];
+    }
+    
+    cout << endl;
+}
