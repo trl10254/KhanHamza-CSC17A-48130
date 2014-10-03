edaedfec377b1e3ce499f4428b5c6c87eb3be5f7
trl10254 <hamzajkhan@gmail.com>
2014-10-02 17:20:35 -0700

    Signed-off-by: trl10254 <hamzajkhan@gmail.com>

--- Hmwk/Chapter 12 Homework/main.cpp
+++ Hmwk/Chapter 12 Homework/main.cpp
@@ -1,19 +1,32 @@
 /* 
- * File:   main.cpp
- * Author: rcc
+ * File:   Chapter 12 Homework
+ * Author: Hamza Khan
  *
  * Created on October 2, 2014, 4:32 PM
  */
 
 #include <cstdlib>
-
+#include <iomanip>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+void Chapter12Problem1();
 
+int main()
+{
+    
     return 0;
+}
+
+//Chapter 12 Problem 1:Write a program that asks the user for the name of a 
+//file. The program should display the first 10 lines of the file on the screen 
+//(the "head" of the file).If the the file has fewer than 10 lines, the entire 
+//file should be displayed, with a message indicating the entire file has been 
+//displayed.
+
+void Chapter12Problem1()
+{
+    
 }
 
