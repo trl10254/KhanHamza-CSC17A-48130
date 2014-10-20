ab5509389870c9033c22fa421cd6ed3975ba03d7
trl10254 <hamzajkhan@gmail.com>
2014-10-20 10:54:49 -0700

    Signed-off-by: trl10254 <hamzajkhan@gmail.com>

---
+++ Proj/Game Project (Windows edition)/main.cpp
@@ -0,0 +1,219 @@
+/* 
+ * File:   main.cpp
+ * Author: Hamza Khan
+ *
+ * Created on October 13, 2014, 7:16 PM
+ */
+
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+//Declare structure
+struct charactercreator
+{
+    string firstname;
+    string lastname;
+};
+
+struct inventory
+{
+    string weapon;
+    string potions;
+    string armor;
+};
+
+//Declare function prototypes
+int weaponstarter(int *, int *, int *);
+void starterguard(int);
+void display(int, int);
+int death(int , int);
+inventory getInfo();
+
+
+int main()
+{
+    //Character profile in a structure
+    charactercreator creation;
+    
+    //Declare local variables
+    string firstname;
+    string lastname;
+    int choice;
+    int baseHP = 30;
+    int maxHP = 120;
+    
+    //Set up the back story to the adventure
+    cout << "There was a legend a told by the native Americans saying that ";
+    cout << "every time you go to sleep at night your soul travels to ";
+    cout << "different worlds and that in the mornings it travels back to your ";
+    cout << "body, but if it doesn't get back to your body in time it is trapped ";
+    cout << "in that world until nightfall. Until your soul doesn't get back to ";
+    cout << "your body you are left in a coma and after three days you and your ";
+    cout << "soul start to die." << endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    cout << "You: Night guys I'm gonna go to sleep." << endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    //Opening scene
+    cout << "Tap. . . Tap. . . Tap" << endl;
+    cout << "Mysterious voice: Get up stranger or else you'll be in danger. ";
+    cout << "Get up now!"<< endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    cout << "You: Ugh. Where am I and what's up with the bright light?" << endl;
+   system("PAUSE");
+    cout << " " << endl << endl;
+    
+    cout << "Mysterious person: We need to hurry up or else you'll be captured. ";
+    cout << "Your our last hope of survival. Hurry up" << endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+  
+    cout << "Both you and the mysterious person hide in a near by bush." << endl;
+    cout << "Then a horse with a rider on top of it came out of a bush with ";
+    cout << "10 guards following it." << endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    cout << "Horse rider: Look around everywhere look under ever rock, bush, ";
+    cout << "cave and tree. If you have to burn this entire forest down, but ";
+    cout << "find the chosen one." << endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    cout << "You get a closer look at the mysterious person who saved you and ";
+    cout << "reminds you of someone you know." << endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    cout << "You: Hey are you by any chance Dumbuldore because you look a lot like him.";
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    //Get the players name
+    cout << "Mysterious person: Who the hell is Dumbuldore, but forget about that. ";
+    cout << "I need to know your name to know whether to not to save you.";
+    cout << "What is your first name: ";
+    getline(cin,creation.firstname);
+    
+    cout << "What is your last name: ";
+    getline(cin,creation.lastname);
+    
+    cout << "Mysterious person: So your name is " << creation.firstname << " ";
+    cout << creation.lastname << endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    cout << "Mysterious person: You are the one the prophecy talked about follow ";
+    cout << "me." << endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    cout << "Guard: Looks like I just hit the jackpot capturing both the chosen ";
+    cout << "one and the rogue wizard Nucktuck" << endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    cout << "Nucktuck: Well " << creation.firstname << " I must take your leave. ";
+    cout << "I leave you with a sword and a stick. Good Luck and find me if you ";
+    cout << "live. Every man for himself!" << endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    //Goes to the menu to select a weapon
+    weaponstarter(*, *, *);
+    
+    cout << "Your weapon of choice is now in your inventory." << endl;
+    system("PAUSE");
+    cout << " " << endl;
+    
+    //Guard taunt
+    cout << "Guard: So you can beat me with that weapon. I laugh at that ";
+    cout << "chosen one. Let's fight." << endl;
+    system("PAUSE");
+    cout << " " << endl << endl;
+    
+    //Goes to the attack menu
+    cout << 
+  
+}
+
+int weaponstarter (int *a, int *b, int *c)
+{
+    //Declare local variables
+    int choice;
+    *a = 0;
+    *b = 0;
+    *c = 0;
+    
+    //Starter weapons
+    cout << "Please choose a weapon of your liking" << endl;
+    cout << "1. Basic Sword" << endl;
+    cout << "Attack: 3" << endl;
+    cout << "Defense: 5" << endl << endl;
+    
+    cout << "2. Basic Stick" << endl;
+    cout << "Attack: 8" << endl;
+    cout << "Defense: 3" << endl << endl;
+    
+    cin >> choice;
+    
+    do
+    {
+        switch(choice)
+        {
+            case 1: 
+                cout << "You have chosen the noble sword. Good luck.";
+                break;
+                
+            case 2:
+                cout << "You have chosen the mighty stick. Good luck.";
+                break;
+                
+            default:
+                cout << "That is in invalid choice please chose either enter 1 or 2";
+                break;
+        }
+    }
+    while (choice < 1 || choice > 3);
+    
+    if (choice == 1)
+    {
+        *a = 1;
+        *b = 3;
+        *c = 5;
+    }
+    
+    else if (choice == 2)
+    {
+        *a = 4;
+        *b = 8;
+        *c = 3;        
+    }
+    
+    return(*a, *b, *c);
+}
+
+inventory getInfo()
+{
+    //Declare local variables
+    int weapons;
+    int potions;
+    int armor;
+    int choice;
+    
+    //Inventory screen
+    cout << "Welcome to the inventory." << endl;
+    cout << "1. Weapons" << endl;
+    cout << "2. Potions" << endl;
+    cout << "3. Armor" << endl;
+    
+}
+
--- Proj/Game Project/nbproject/Makefile-variables.mk
+++ Proj/Game Project/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-MacOSX
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Debug=game_project
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/game_project
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/game_project
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Debug=gameproject.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/gameproject.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/gameproject.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-MacOSX
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
