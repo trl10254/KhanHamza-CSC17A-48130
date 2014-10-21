13f92bca11033661acdf158027b490d77292cfd1
trl10254 <hamzajkhan@gmail.com>
2014-10-21 12:29:31 -0700

    Signed-off-by: trl10254 <hamzajkhan@gmail.com>

--- Proj/Game Project (Windows edition)/main.cpp
+++ Proj/Game Project (Windows edition)/main.cpp
@@ -25,18 +25,25 @@ struct inventory
     string armor;
 };
 
+struct weaponselection
+{
+    int attack;
+    int defence;
+    int speed;
+};
+
 //Declare function prototypes
-int weaponstarter(int *, int *, int *);
 void starterguard(int);
 void display(int, int);
 int death(int , int);
 inventory getInfo();
-
+weaponselection weaponchoice(weaponselection);
 
 int main()
 {
     //Character profile in a structure
     charactercreator creation;
+    weaponselection weapon;
     
     //Declare local variables
     string firstname;
@@ -44,6 +51,9 @@ int main()
     int choice;
     int baseHP = 30;
     int maxHP = 120;
+    weapon.attack = 0;
+    weapon.defence = 0;
+    weapon.speed = 0;
     
     //Set up the back story to the adventure
     cout << "There was a legend a told by the native Americans saying that ";
@@ -53,48 +63,48 @@ int main()
     cout << "in that world until nightfall. Until your soul doesn't get back to ";
     cout << "your body you are left in a coma and after three days you and your ";
     cout << "soul start to die." << endl;
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     cout << "You: Night guys I'm gonna go to sleep." << endl;
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     //Opening scene
     cout << "Tap. . . Tap. . . Tap" << endl;
     cout << "Mysterious voice: Get up stranger or else you'll be in danger. ";
     cout << "Get up now!"<< endl;
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     cout << "You: Ugh. Where am I and what's up with the bright light?" << endl;
-   system("PAUSE");
+   system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     cout << "Mysterious person: We need to hurry up or else you'll be captured. ";
     cout << "Your our last hope of survival. Hurry up" << endl;
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
   
     cout << "Both you and the mysterious person hide in a near by bush." << endl;
     cout << "Then a horse with a rider on top of it came out of a bush with ";
     cout << "10 guards following it." << endl;
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     cout << "Horse rider: Look around everywhere look under ever rock, bush, ";
     cout << "cave and tree. If you have to burn this entire forest down, but ";
     cout << "find the chosen one." << endl;
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     cout << "You get a closer look at the mysterious person who saved you and ";
     cout << "reminds you of someone you know." << endl;
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     cout << "You: Hey are you by any chance Dumbuldore because you look a lot like him.";
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     //Get the players name
@@ -108,50 +118,46 @@ int main()
     
     cout << "Mysterious person: So your name is " << creation.firstname << " ";
     cout << creation.lastname << endl;
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     cout << "Mysterious person: You are the one the prophecy talked about follow ";
     cout << "me." << endl;
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     cout << "Guard: Looks like I just hit the jackpot capturing both the chosen ";
     cout << "one and the rogue wizard Nucktuck" << endl;
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     cout << "Nucktuck: Well " << creation.firstname << " I must take your leave. ";
     cout << "I leave you with a sword and a stick. Good Luck and find me if you ";
     cout << "live. Every man for himself!" << endl;
-    system("PAUSE");
+   system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl << endl;
     
     //Goes to the menu to select a weapon
-    weaponstarter(*, *, *);
+    weaponchoice(weapon);
     
     cout << "Your weapon of choice is now in your inventory." << endl;
-    system("PAUSE");
+    system("read -p \"Press enter to continue\" -n 1 -s");
     cout << " " << endl;
     
     //Guard taunt
     cout << "Guard: So you can beat me with that weapon. I laugh at that ";
     cout << "chosen one. Let's fight." << endl;
-    system("PAUSE");
-    cout << " " << endl << endl;
+    system("read -p \"Press enter to continue\" -n 1 -s");
+    cout << " " << endl << endl; 
     
-    //Goes to the attack menu
-    cout << 
-  
+    return 0;
 }
 
-int weaponstarter (int *a, int *b, int *c)
+
+ weaponselection weaponchoice(weaponselection weapon)
 {
-    //Declare local variables
+    //Declare local variable
     int choice;
-    *a = 0;
-    *b = 0;
-    *c = 0;
     
     //Starter weapons
     cout << "Please choose a weapon of your liking" << endl;
@@ -186,19 +192,19 @@ int weaponstarter (int *a, int *b, int *c)
     
     if (choice == 1)
     {
-        *a = 1;
-        *b = 3;
-        *c = 5;
+        weapon.speed = 1;
+        weapon.attack = 3;
+        weapon.defence = 5;
     }
     
     else if (choice == 2)
     {
-        *a = 4;
-        *b = 8;
-        *c = 3;        
+        weapon.speed = 4;
+        weapon.attack = 8;
+        weapon.defence = 3;        
     }
     
-    return(*a, *b, *c);
+    return weapon;
 }
 
 inventory getInfo()
